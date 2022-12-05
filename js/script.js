27d54032270af6f7b2697e72d968bd5f395277ed
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-12-05 11:50:55 +0100

    Add array obj

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 // 1.** Creare un oggetto che descriva una studentessa o uno studente, con le seguenti proprietà: `nome`, `cognome` e `età`.
 // 2.** Stampare a schermo attraverso un ciclo `for-in` tutte le proprietà dell'oggetto.
+// // 3. Creare un array di oggetti di studenti (ossìa oggetti con le proprietà sopra descritte) e mostriamoli.
+
 
 const mainElement = document.querySelector("main");
 
-const hElement = document.createElement('h1')
+const hElement = document.createElement('h1');
+
+const studentList = [
+    {
+       name : 'Giggi',
+       surname : 'Giggetto',
+       age : 25
+    },
+    {
+        name : 'Francesco',
+        surmane : 'Franceschi',
+        age : 20
+    },
+    {
+        name : 'Vincenzo',
+        surmane : 'Vincenti',
+        age : 50
+    }
+];
 
 const student = {
     name : 'Claudio',
@@ -14,4 +34,8 @@ const student = {
 for (let key in student){
     hElement.append(' ', key, ' => ' , student[key])
     mainElement.append(hElement);
+}
+
+for ( let i = 0 ; i<studentList.length ; i++){
+    console.log (studentList[i]);
 }
